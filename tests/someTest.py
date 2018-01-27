44fd9a221e5812cb4be8deef2a516e307ac3b632
jelleas <jelle.van.assema@gmail.com>
2018-01-27 15:13:50 +0100

    sandboxTest

--- tests/someTest.py
+++ tests/someTest.py
@@ -5,7 +5,7 @@ import checkpy.assertlib as asserts
 @t.test(10)
 def exactlyFoo(test):
 	def testMethod():
-		output = lib.outputOf(_fileName, overwriteAttributes = [("__name__", "__main__")])
+		output = lib.outputOf(test.fileName, overwriteAttributes = [("__name__", "__main__")])
 		return asserts.exact(output.strip(), "foo")
 
 	test.test = testMethod
